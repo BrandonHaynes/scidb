015b1692067ac4399e66afe5b1dbdc070786c592
Brandon Haynes <bhaynes@cs.washington.edu>
2015-05-28 10:31:56 -0700

    Added explicit ranges to binning aggregates

--- examples/bin/bin.cpp
+++ examples/bin/bin.cpp
@@ -21,6 +21,18 @@ EXPORTED_FUNCTION const vector<AggregatePtr>& GetAggregates()
     return _aggregates;
 }
 
+AGGBIN(1, 10);
+AGGBIN(2, 10);
+AGGBIN(4, 10);
+AGGBIN(8, 10);
+AGGBIN(16, 10);
+AGGBIN(32, 10);
+AGGBIN(64, 10);
+AGGBIN(128, 10);
+AGGBIN(256, 10);
+AGGBIN(512, 10);
+AGGBIN(1024, 10);
+
 static class BinLibrary
 {
 public:
@@ -28,7 +40,17 @@ public:
     BinLibrary()
     {
         // Aggregates
-      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin, double>("bin", TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin1,    double>("bin1",       1, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin2,    double>("bin2",       2, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin4,    double>("bin4",       4, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin8,    double>("bin8",       8, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin16,   double>("bin16",     16, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin32,   double>("bin32",     32, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin64,   double>("bin64",     64, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin128,  double>("bin128",   128, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin256,  double>("bin256",   256, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin512,  double>("bin512",   512, TypeLibrary::getType(TID_DOUBLE))));
+      _aggregates.push_back(AggregatePtr(new BinAggregate<AggBin1024, double>("bin1024", 1024, TypeLibrary::getType(TID_DOUBLE))));
       _aggregates.push_back(make_shared<SignedCountAggregate>(TypeLibrary::getType(TID_VOID)));
     }
 
--- examples/bin/bin_functions.h
+++ examples/bin/bin_functions.h
@@ -4,58 +4,50 @@
 #include <query/Aggregate.h>
 
 template<template <typename TS, typename TSR> class A, typename T, bool asterisk = false>
-  class BinAggregate: public BaseAggregate<A, T, int64_t, asterisk>
+class BinAggregate: public BaseAggregate<A, T, int64_t, asterisk>
 {
-public:
-    BinAggregate(const std::string& name, Type const& aggregateType):
-BaseAggregate<A, T, int64_t, asterisk>(name, aggregateType, TypeLibrary::getType(TID_INT64))
-    {}
-
-   virtual bool isOrderSensitive()
-   {
-   return true;
-   }
+ public:
+    BinAggregate(const std::string& name, const int64_t range, Type const& aggregateType):
+        BaseAggregate<A, T, int64_t, asterisk>(name, aggregateType, TypeLibrary::getType(TID_INT64)), range(range)
+    { }
 };
 
-
-template <typename TS, typename TSR>
+template <typename TS, typename TSR, int64_t range, int64_t bins>
 class AggBin
 {
  public:
   struct State {
-    TSR _sum;
-    int64_t _count;
+    TSR _value;
+    bool _hasValue;
   };
 
   static void init(State& state)
   {
-    state._sum = TSR();
-    state._count = 10;
+    state._hasValue = false;
+    state._value = TSR();
   }
 
   static void aggregate(State& state, const TS& value)
   {
-    state._sum += static_cast<TSR>(value);
-    state._count++;
+    state._value = state._hasValue ? static_cast<TSR>(value) - state._value : static_cast<TSR>(value);
+    state._hasValue = true;
   }
 
-  static void multAggregate(State& state, const TS& value, int64_t count)
+  static void multAggregate(State& state, const TS& value, uint64_t count)
   {
-    state._sum += static_cast<TSR>(value) * count;
-    state._count += count;
+    aggregate(state, static_cast<TSR>(value));
   }
 
   static void merge(State& state, const State& new_state)
   {
-    state._sum += new_state._sum;
-    state._count += new_state._count;
   }
 
   static bool final(const State& state, TSR& result)
   {
-    if (state._count == 0)
-      return false;
-    result = static_cast<TSR>(state._sum) / state._count;
+    int64_t low = -range;
+    int64_t high = range;
+    result = (int64_t)((double)(bins - 1) * (state._value - low) / (high - low));
+
     return true;
   }
 
@@ -65,109 +57,114 @@ class AggBin
   }
 };
 
+#define AGGBIN(range, bins)                                 \
+template <typename TS, typename TSR>                        \
+  class AggBin##range: public AggBin<TS, TSR, range, bins>  \
+{ }                                                         \
+
 class SignedCountAggregate : public CountingAggregate
 {
-private:
-    bool _ignoreNulls;
+ private:
+  bool _ignoreNulls;
 
-protected:
-    void accumulate(Value& dstState, Value const& srcValue)
-    {
-        assert(isStateInitialized(dstState));
-        assert(isAccumulatable(srcValue));
+ protected:
+  void accumulate(Value& dstState, Value const& srcValue)
+  {
+    assert(isStateInitialized(dstState));
+    assert(isAccumulatable(srcValue));
 
-        (*dstState.getData<int64_t>())++;
-    }
+    (*dstState.getData<int64_t>())++;
+  }
 
-    void merge(Value& dstState, Value const& srcState)
-    {
-        assert(isStateInitialized(dstState));
-        assert(isMergeable(srcState));
+  void merge(Value& dstState, Value const& srcState)
+  {
+    assert(isStateInitialized(dstState));
+    assert(isMergeable(srcState));
 
-        (*dstState.getData<int64_t>()) += srcState.getInt64();
-    }
+    (*dstState.getData<int64_t>()) += srcState.getInt64();
+  }
 
-public:
-    SignedCountAggregate(Type const& aggregateType):
-        CountingAggregate("signed_count", aggregateType, TypeLibrary::getType(TID_INT64)), _ignoreNulls(aggregateType.typeId() != TID_VOID)
+ public:
+ SignedCountAggregate(Type const& aggregateType):
+  CountingAggregate("signed_count", aggregateType, TypeLibrary::getType(TID_INT64)), _ignoreNulls(aggregateType.typeId() != TID_VOID)
     {}
 
-    AggregatePtr clone() const
-    {
-        return AggregatePtr(new SignedCountAggregate(getAggregateType()));
+  AggregatePtr clone() const
+  {
+    return AggregatePtr(new SignedCountAggregate(getAggregateType()));
+  }
+
+  AggregatePtr clone(Type const& aggregateType) const
+  {
+    return AggregatePtr(new SignedCountAggregate(aggregateType));
+  }
+
+  Type getStateType() const
+  {
+    return TypeLibrary::getType(TID_INT64);
+  }
+
+  bool supportAsterisk() const
+  {
+    return true;
+  }
+
+  void initializeState(Value& state)
+  {
+    state = Value(getStateType());
+    state = TypeLibrary::getDefaultValue(getStateType().typeId());
+  }
+
+  bool ignoreNulls() const
+  {
+    return _ignoreNulls;
+  }
+
+  bool needsAccumulate() const
+  {
+    //if _ignoreNulls is true, that means this is a "count(attribute_name)" aggregate.
+    //this can be made more optimal but right now we are disabling the optimization to fix the bug
+    //aggregate(build(<v:double null>[i=1:2,2,0],null), count(v)) --> 0
+    return _ignoreNulls;
+  }
+
+  void accumulateIfNeeded(Value& state, ConstRLEPayload const* tile)
+  {
+    if (! isStateInitialized(state)) {
+      initializeState(state);
+      assert(isStateInitialized(state));
     }
 
-    AggregatePtr clone(Type const& aggregateType) const
-    {
-        return AggregatePtr(new SignedCountAggregate(aggregateType));
+    if (!ignoreNulls()) {
+      *state.getData<int64_t>() += tile->count();
+    } else {
+      ConstRLEPayload::iterator iter = tile->getIterator();
+      while (!iter.end())
+  {
+    if (!iter.isNull())
+      {
+        *state.getData<int64_t>() += iter.getSegLength();
+      }
+    iter.toNextSegment();
+  }
     }
+  }
 
-    Type getStateType() const
-    {
-        return TypeLibrary::getType(TID_INT64);
+  void overrideCount(Value& state, uint64_t newCount)
+  {
+    *state.getData<int64_t>() = (int64_t)newCount;
+  }
+
+  void finalResult(Value& dstValue, Value const& srcState)
+  {
+    if (! isMergeable(srcState))
+      {
+  dstValue = TypeLibrary::getDefaultValue(getResultType().typeId());
+      }
+    else {
+      dstValue = srcState;
     }
-
-    bool supportAsterisk() const
-    {
-        return true;
-    }
-
-    void initializeState(Value& state)
-    {
-        state = Value(getStateType());
-        state = TypeLibrary::getDefaultValue(getStateType().typeId());
-    }
-
-    bool ignoreNulls() const
-    {
-        return _ignoreNulls;
-    }
-
-    bool needsAccumulate() const
-    {
-        //if _ignoreNulls is true, that means this is a "count(attribute_name)" aggregate.
-        //this can be made more optimal but right now we are disabling the optimization to fix the bug
-        //aggregate(build(<v:double null>[i=1:2,2,0],null), count(v)) --> 0
-        return _ignoreNulls;
-    }
-
-    void accumulateIfNeeded(Value& state, ConstRLEPayload const* tile)
-    {
-        if (! isStateInitialized(state)) {
-            initializeState(state);
-            assert(isStateInitialized(state));
-        }
-
-        if (!ignoreNulls()) {
-            *state.getData<int64_t>() += tile->count();
-        } else {
-            ConstRLEPayload::iterator iter = tile->getIterator();
-            while (!iter.end())
-            {
-                if (!iter.isNull())
-                {
-                    *state.getData<int64_t>() += iter.getSegLength();
-                }
-                iter.toNextSegment();
-            }
-        }
-    }
-
-    void overrideCount(Value& state, uint64_t newCount)
-    {
-        *state.getData<int64_t>() = (int64_t)newCount;
-    }
-
-    void finalResult(Value& dstValue, Value const& srcState)
-    {
-        if (! isMergeable(srcState))
-        {
-            dstValue = TypeLibrary::getDefaultValue(getResultType().typeId());
-        }
-        else {
-            dstValue = srcState;
-        }
-    }
+  }
 };
 
 #endif // BIN_FUNCTIONS_H
