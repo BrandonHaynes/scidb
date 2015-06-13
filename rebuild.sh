482f6793a8f02f3c67a7c4ef56f9ef7c13365667
Brandon Haynes <bhaynes@cs.washington.edu>
2015-06-13 11:29:38 -0700

    Update utility scripts to 14.12

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 nodelist=$(cat /root/nodes.txt)
 nodes=${nodelist/$'\n'/ }
 
-/opt/scidb/14.8/bin/scidb.py stopall mydb
+/opt/scidb/14.12/bin/scidb.py stopall mydb
 
 #deployment/deploy.sh prepare_toolchain master
 #deployment/deploy.sh prepare_coordinator master
@@ -11,20 +11,20 @@ su scidb -c "./run.py make -j4"
 su scidb -c "./run.py make_packages /tmp/packages -f"
 
 while read node; do
-  scp -r /mnt/scidb/stage/build/debian/scidb-14.8-plugins/mnt/scidb/stage/install/* $node:/opt/scidb/14.8
+  scp -r /mnt/scidb/stage/build/debian/scidb-14.12-plugins/mnt/scidb/stage/install/* $node:/opt/scidb/14.12
 done < /root/nodes.txt
 
 deployment/deploy.sh scidb_install /tmp/packages $nodes
 
-/opt/scidb/14.8/bin/scidb.py stopall mydb
+/opt/scidb/14.12/bin/scidb.py stopall mydb
 
 while read node; do
-  scp -r /mnt/scidb/stage/install/* $node:/opt/scidb/14.8
+  scp -r /mnt/scidb/stage/install/* $node:/opt/scidb/14.12
 done < /root/nodes.txt
 
-/opt/scidb/14.8/bin/scidb.py stopall mydb
+/opt/scidb/14.12/bin/scidb.py stopall mydb
 
 deployment/deploy.sh scidb_prepare scidb "scidb" mydb mydb mydb /mnt/scidb/db 1 default 1 $nodes
 
-/opt/scidb/14.8/bin/scidb.py initall mydb -f
-/opt/scidb/14.8/bin/scidb.py startall mydb
+/opt/scidb/14.12/bin/scidb.py initall mydb -f
+/opt/scidb/14.12/bin/scidb.py startall mydb
--- restartscidb.sh
+++ restartscidb.sh
@@ -1,2 +1,2 @@
-/opt/scidb/14.8/bin/scidb.py stopall mydb
-/opt/scidb/14.8/bin/scidb.py startall mydb
+/opt/scidb/14.12/bin/scidb.py stopall mydb
+/opt/scidb/14.12/bin/scidb.py startall mydb
